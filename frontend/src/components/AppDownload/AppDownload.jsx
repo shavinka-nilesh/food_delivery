6ba90f54680080c250eedf9fcfec30d8ee8bd302
Shavinka_Nilesh <shavinkanilesh@gmail.com>
2026-03-08 23:24:40 +0530

    Make Playstore Icons Clickable

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -7,8 +7,12 @@ const AppDownload = () => {
         <div className='app-download' id='app-download'>
             <p>For Better Experience Download <br />Tomato App</p>
             <div className="app-download-platforms">
-                <img src={assets.play_store} alt="" />
-                <img src={assets.app_store} alt="" />
+                <a href="https://play.google.com/store/" target="_blank" rel="noopener noreferrer">
+                    <img src={assets.play_store} alt="Play Store" />
+                </a>
+                <a href="https://www.apple.com/app-store/" target="_blank" rel="noopener noreferrer">
+                    <img src={assets.app_store} alt="App Store" />
+                </a>
             </div>
         </div>
     )
